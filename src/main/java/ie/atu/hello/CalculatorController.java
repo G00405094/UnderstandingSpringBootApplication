ef905e73f299fa88fe7cd61451ddd06d77746903
G00405094 <G00405094@atu.ie>
2023-10-04 00:39:02 +0100

    Created CalculatorController class using PathVariable,

--- src/main/java/ie/atu/hello/CalculatorController.java
+++ src/main/java/ie/atu/hello/CalculatorController.java
@@ -1,14 +1,15 @@
 package ie.atu.hello;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 
 public class CalculatorController {
-    @GetMapping("/calculate")
-    public int calculate(@RequestParam int num1, @RequestParam int num2, @RequestParam String operation){
+    @GetMapping("/calculate/{num1}/{num2}/{operation}")
+    public int calculate(@PathVariable int num1, @PathVariable int num2, @PathVariable String operation){
         if (operation.equals("add")) {
             return num1 + num2;
         } else if (operation.equals("multiply")) {
